5b61f41f4085d9d8787d25d23cbdaafaa5b6c448
Musa17 <amusul00@fesb.hr>
2021-10-16 16:26:52 +0200

    Creating TodoListViewModel class

---
+++ ToDoMobileApp/ToDoMobileApp/TodoListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ToDoMobileApp
+{
+    public class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            TodoItems.Add(new TodoItem("Todo1", false));
+            TodoItems.Add(new TodoItem("Todo2", false));
+            TodoItems.Add(new TodoItem("Todo3", false));
+        }
+    }
+}
